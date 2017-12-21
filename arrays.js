dec004ddcc13d8d0159ebc5fa99d8e53c04e9cdc
David McGrann <dpmcgrann@gmail.com>
2017-12-21 17:14:53 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -32,7 +32,4 @@ function destructivelyRemoveElementFromEndOfArray(array, element) {
   array.pop(element)
   return array
 }
-function removeElementFromEndOfArray(array, element) {
-  array.slice(-1)
-  return array[]
-}
+
